709928d6cf9b7247b22e82034cf003e5b24c495d
JojoAmanny <joshuaelishagyubok@gmail.com>
2023-03-06 13:41:39 -0800

    added wa float and scroll to top

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,34 @@ $(function () {
 $(window).on('load', function() {
     $('.book-flight-area').addClass('fadeInUp');
 });
+
+// For customized dropdown
+function getdropdown(id) {
+    // alert(id);
+    $('#dropspan'+id).toggleClass('fa-angle-down fa-angle-right');
+    if($('#dropspan'+id).hasClass('fa-angle-down')) {
+        $('#dropcontent'+id).slideDown();
+    } else if($('#dropspan'+id).hasClass('fa-angle-right')) {
+        $('#dropcontent'+id).slideUp();
+    }
+}
+
+ //Get the button
+ var mybutton = document.getElementById("myBtn");
+
+ // When the user scrolls down 20px from the top of the document, show the button
+ window.onscroll = function() {scrollFunction()};
+ 
+ function scrollFunction() {
+   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+     mybutton.style.display = "block";
+   } else {
+     mybutton.style.display = "none";
+   }
+ }
+ 
+ // When the user clicks on the button, scroll to the top of the document
+ function topFunction() {
+   document.body.scrollTop = 0;
+   document.documentElement.scrollTop = 0;
+ }
